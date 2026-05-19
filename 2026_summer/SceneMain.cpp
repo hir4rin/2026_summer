e1fde4db1871c30aa7afc70b7ff26ecc06ef7398
ひらお <fko2547051@stu.o-hara.ac.jp>
2026-05-19 16:38:53 +0900

    up

---
+++ 2026_summer/SceneMain.cpp
@@ -0,0 +1,50 @@
+﻿#include "SceneMain.h"
+#include "DxLib.h"
+
+SceneMain::SceneMain():
+m_frameCount(0)
+{
+}
+
+SceneMain::~SceneMain()
+{
+}
+
+void SceneMain::Init()
+{
+	
+
+
+}
+
+void SceneMain::Update()
+{
+	m_frameCount++;
+}
+
+void SceneMain::Draw()
+{
+	DrawGrid();
+	DrawString(0, 0, "SceneMain", GetColor(255, 255, 255));
+	DrawFormatString(0, 16, GetColor(255, 255, 255), "FRAME:%d", m_frameCount);
+}
+
+void SceneMain::DrawGrid()
+{
+	// 直線の始点と終点
+	VECTOR startPos;
+	VECTOR endPos;
+
+	for (int z = -300; z <= 300; z += 100)
+	{
+		startPos = VGet(-300.0f, 0.0f, static_cast<float>(z));
+		endPos = VGet(300.0f, 0.0f, static_cast<float>(z));
+		DrawLine3D(startPos, endPos, 0xff0000);
+	}
+	for (int x = -300; x <= 300; x += 100)
+	{
+		startPos = VGet(static_cast<float>(x), 0.0f, -300.0f);
+		endPos = VGet(static_cast<float>(x), 0.0f, 300.0f);
+		DrawLine3D(startPos, endPos, 0x0000ff);
+	}
+}
