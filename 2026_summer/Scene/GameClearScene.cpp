a05235235d91a7ce57b6d36b4fe557595511f76f
ひらお <fko2547051@stu.o-hara.ac.jp>
2026-07-10 13:49:41 +0900

    うｐ

--- 2026_summer/Scene/GameClearScene.cpp
+++ 2026_summer/Scene/GameClearScene.cpp
@@ -1,7 +1,12 @@
 ﻿#include "GameClearScene.h"
+#include "../Input.h"
+#include "GameScene.h"
+#include "SceneController.h"
 
-GameClearScene::GameClearScene(SceneController& controller)
+GameClearScene::GameClearScene(SceneController& controller) :Scene(controller)
 {
+	m_updateFunc = static_cast<UpdateFunc_t>(&GameClearScene::NormalUpdate);
+	m_drawFunc = static_cast<DrawFunc_t>(&GameClearScene::NormalDraw);
 }
 
 GameClearScene::~GameClearScene()
@@ -10,6 +15,7 @@ GameClearScene::~GameClearScene()
 
 void GameClearScene::Update()
 {
+	(this->*m_updateFunc)();
 }
 
 void GameClearScene::FadeInUpdate()
@@ -18,6 +24,14 @@ void GameClearScene::FadeInUpdate()
 
 void GameClearScene::NormalUpdate()
 {
+	auto& input = Input::GetInstance();
+
+
+	if (input.IsTriggered("A"))
+	{
+		//ゲームシーンに遷移する
+		m_controller.ChangeScene(std::make_shared<GameScene>(m_controller));
+	}
 }
 
 void GameClearScene::FadeOutUpdate()
@@ -26,6 +40,7 @@ void GameClearScene::FadeOutUpdate()
 
 void GameClearScene::Draw()
 {
+	(this->*m_drawFunc)();
 }
 
 void GameClearScene::FadeInDraw()
@@ -34,6 +49,7 @@ void GameClearScene::FadeInDraw()
 
 void GameClearScene::NormalDraw()
 {
+	DrawFormatString(300, 300, GetColor(255, 255, 255), "GameClearScene");
 }
 
 void GameClearScene::FadeOutDraw()
